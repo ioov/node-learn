561d4c83e9e6a3aed4e4e56ac7c2bc4dd871054d
koala4js <koala4js@gmail.com>
2016-08-25 19:18:59 +0800

    up

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ http.createServer(function(req, res){
 	res.writeHead("content-type", "text/plain");
 	res.write("hello world");
 	res.end();
-}).listen(8080);
+}).listen(process.env.LEANCLOUD_APP_PORT);
