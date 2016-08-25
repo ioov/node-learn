183756f1e7a718780125554812a08902f2fd26e3
koala4js <koala4js@gmail.com>
2016-08-25 19:42:48 +0800

    up

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
 var http = require("http");
-var AV = require('leanengine');
 var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || 3000);
-
-AV.init({
-	appId: process.env.LEANCLOUD_APP_ID,
-	appKey: process.env.LEANCLOUD_APP_KEY,
-	masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
-});
-
-// 如果不希望使用 masterKey 权限，可以将下面一行删除
-AV.Cloud.useMasterKey();
 
 http.createServer(function(req, res) {
 	res.writeHead("content-type", "text/plain");
