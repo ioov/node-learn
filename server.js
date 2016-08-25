a0e7dd9cb208dcbc4450818ee11c37e12f86e661
koala4js <koala4js@gmail.com>
2016-08-25 19:25:13 +0800

    up

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require("http");
+var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || 3000);
 
 http.createServer(function(req, res){
 	res.writeHead("content-type", "text/plain");
 	res.write("hello world");
 	res.end();
-}).listen(process.env.LEANCLOUD_APP_PORT);
+}).listen(PORT);
