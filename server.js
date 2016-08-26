5219585656a8e6b110fe95273e4c6f3a6b1953c4
koala4js <koala4js@gmail.com>
2016-08-26 10:12:17 +0800

    up

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var http = require("http");
+
+// 端口一定要从环境变量 `LEANCLOUD_APP_PORT` 中获取。
+// LeanEngine 运行时会分配端口并赋值到该变量。
 var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || 3000);
 
-console.log(PORT);
-// http.createServer(function(req, res) {
-// 	res.writeHead("content-type", "text/plain");
-// 	res.write("hello world");
-// 	res.end();
-// }).listen(80);
+http.createServer(function(req, res) {
+	res.writeHead("content-type", "text/plain");
+	res.write(PORT);
+	res.end(PORT);
+}).listen(PORT);
