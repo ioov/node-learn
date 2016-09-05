4ce5c90cfd18f4828fc4dfb2292f1efe23a3a94c
koala4js <koala4js@gmail.com>
2016-09-05 19:06:12 +0800

    up

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var AV = require('leanengine');
+// var AV = require('leanengine');
 var http = require("http");
 
-var APP_ID = process.env.LC_APP_ID;
-var APP_KEY = process.env.LC_APP_KEY;
-var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
-AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
-// 如果不希望使用 masterKey 权限，可以将下面一行删除
-AV.Cloud.useMasterKey();
+// var APP_ID = process.env.LC_APP_ID;
+// var APP_KEY = process.env.LC_APP_KEY;
+// var MASTER_KEY = process.env.LC_APP_MASTER_KEY;
+// AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
+// // 如果不希望使用 masterKey 权限，可以将下面一行删除
+// AV.Cloud.useMasterKey();
 
 var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || 3000);
 
